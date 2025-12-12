f611a4e41e6b4305dce035644d7a70fa7f67ec5e
Fathismael93 <fathismael@gmail.com>
2025-12-12 21:44:35 +0300

    Updated login, registration, middleware, auth.js, auth-client

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,6 @@ import winston from 'winston';
 // Création d'un logger structuré
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info', // Niveau par défaut : info
-  defaultMeta: { service: 'api-services' },
 });
 
 export default logger;
