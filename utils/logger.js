f7a50900446fde53878acd073d968ff005250d4d
Fathismael93 <fathismael@gmail.com>
2025-12-12 21:51:53 +0300

    deleted logger.js

--- utils/logger.js
+++
@@ -1,8 +0,0 @@
-import winston from 'winston';
-
-// Création d'un logger structuré
-const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || 'info', // Niveau par défaut : info
-});
-
-export default logger;
