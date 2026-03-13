d5cf483c8700d13b915daf54a946e7cd9cecfd48
Fathismael93 <fathismael@gmail.com>
2026-03-13 15:34:03 +0300

    Implementing add video files for the benew channel

--- ui/pages/channel/VideosList.jsx
+++ ui/pages/channel/VideosList.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-
 const VideosList = () => {
   return (
-    <div className={styles.applicationsContainer}>
-      {/* Header avec recherche et filtres */}
-      <div className={styles.top}>
-        <AppSearch
-          placeholder="Search for an application..."
-          onFilterChange={handleFilterChange}
-          currentFilters={currentFilters}
-        />
-        <AppFilters
-          onFilterChange={handleFilterChange}
-          currentFilters={currentFilters}
-        />
-        <button
-          onClick={() => handleNavigate('/dashboard/applications/add', null)}
-          className={styles.addButton}
-          type="button"
-        >
-          <MdAdd /> Add Application
-        </button>
-      </div>
-    </div>
+    <div></div>
+    // <div className={styles.applicationsContainer}>
+    //   {/* Header avec recherche et filtres */}
+    //   <div className={styles.top}>
+    //     <AppSearch
+    //       placeholder="Search for an application..."
+    //       onFilterChange={handleFilterChange}
+    //       currentFilters={currentFilters}
+    //     />
+    //     <AppFilters
+    //       onFilterChange={handleFilterChange}
+    //       currentFilters={currentFilters}
+    //     />
+    //     <button
+    //       onClick={() => handleNavigate('/dashboard/applications/add', null)}
+    //       className={styles.addButton}
+    //       type="button"
+    //     >
+    //       <MdAdd /> Add Application
+    //     </button>
+    //   </div>
+    // </div>
   );
 };
 
